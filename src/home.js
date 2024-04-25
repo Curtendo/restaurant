37d19215f179ee51e11f7bc51c119499b68236b8
Curtis CHROME <mrmr450@gmail.com>
2024-04-25 13:19:45 +0900

    Implement nav tab CSS best I could

--- src/home.js
+++ src/home.js
@@ -1,5 +1,7 @@
 import Tacos from './beeftacos.jpg';
 
+
+
 const contentDiv = document.querySelector("#content");
 
 function addHomeElements(contentDiv) {
@@ -7,16 +9,16 @@ function addHomeElements(contentDiv) {
     newH1.classList.add("title");
     newH1.textContent = "Tacos in Japan";
 
-    const tacosPic = new Image();
-    tacosPic.src = Tacos;
-    tacosPic.width = "500";
-
     const picAndTextDiv = document.createElement("div");
     picAndTextDiv.classList.add("pic-and-text");
 
+    const tacosPic = new Image();
+    tacosPic.src = Tacos;
+    tacosPic.width = "400";
+
     const welcomeDiv = document.createElement("div");
     welcomeDiv.classList.add("welcome-message");
-    welcomeDiv.textContent = "Welcome to tacos in Japan";
+    welcomeDiv.textContent = "Welcome to Tacos in Japan. Have you ever had any tacos in Japan? They aren't very good, right? That's why when you come to Tacos in Japan, you getting actual, authentic, flavor-full tacos, every time.";
 
     contentDiv.appendChild(newH1);
     contentDiv.appendChild(picAndTextDiv);
