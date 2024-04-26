9371e438a1d2d213c382e5bc21d82c52f9fe6b68
Curtis CHROME <mrmr450@gmail.com>
2024-04-26 16:09:35 +0900

    Remove public path to hopefully fix file links

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    
     assetModuleFilename: 'images/[hash][ext][query]'
   },
   module: {
